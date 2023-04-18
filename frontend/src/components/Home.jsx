31382dbbd156526bad66e9b82275e2030e75fe8f
shivam1903 <shivam234agarwal@gmail.com>
2023-04-19 01:26:52 +0530

    Adding the drop-down and integer input

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Container, Row, Col, Form, Collapse, Alert, Spinner } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import './Home.css';
 
 const Home = () => {
 
@@ -11,6 +12,14 @@ const Home = () => {
     const [fileUploaded, setFileUploaded] = useState(false)
     const [transcriptionResult, setTranscriptionResult] = useState(null)
     const [processing, setProcessing] = useState(false)
+    const [open1, setOpen1] = React.useState(false);
+    const [open2, setOpen2] = React.useState(false);
+    const [buttonText, setButtonText] = useState(Language1);
+    const [num_spe, set_num_spe] = useState(0);
+
+    var Language1 = 'Language';
+    var Size1 = '';
+    var Number_Speakers = 0;
 
     const updateSelectedFile = (e) => {
         setSelectedFile(e.target.files[0])
@@ -45,6 +54,70 @@ const Home = () => {
         setProcessing(false)
     }
 
+    const handleOpen1 = () => {
+        setOpen1(!open1);
+        setButtonText(Language1);
+
+    };
+
+    const  handleOpen2 = () => {
+        setOpen2(!open2);
+    }
+
+    const LanguageEnglish = () => {
+        // do something
+        Language1 = 'English';
+        // setButtonText('English');
+        setOpen1(false);
+    };
+
+    const LanguageAny = () => {
+        // do something
+        Language1 = 'Any';
+        setOpen1(false);
+    };
+
+    const SizeTiny = () => {
+        // do something
+        Size1 = 'Tiny';
+        setOpen2(false);
+    };
+
+    const SizeBase = () => {
+        // do something
+        Size1 = 'Base';
+        setOpen2(false);
+    };
+
+    const SizeSmall = () => {
+        // do something
+        Size1 = 'Small';
+        setOpen2(false);
+    };
+
+    const SizeMedium = () => {
+        // do something
+        Size1 = 'Medium';
+        setOpen2(false);
+    };
+
+    const SizeLarge = () => {
+        // do something
+        Size1 = 'Large';
+        setOpen2(false);
+    };
+
+    // const Num_Speakers = () =>{
+    //     Number_Speakers = num_spe;
+    //     // console.log(Number_Speakers);
+    // }
+
+    const Num_Speakers = (e) => {
+        set_num_spe(e.target.value)
+        Number_Speakers = e.target.value;
+    }
+
+
     return (
         <div>
             <h1 style={{ padding : 40 }}>Information Retrieval from Audio Files</h1>
@@ -62,6 +135,56 @@ const Home = () => {
                                         <Form.Control type='file' onChange={updateSelectedFile} accept='audio/*' />
                                     </Form.Group>
                                 </Form>
+
+                                <div className="d-grid gap-2">
+                                    <div className="dropdown">
+                                        <Button onClick={handleOpen1}>Language</Button>
+                                        {open1 ?(
+                                            <ul className="menu">
+                                            <li className="menu-item">
+                                                <Button onClick={LanguageEnglish}>English</Button>
+                                            </li>
+                                            <li className="menu-item">
+                                                <Button onClick={LanguageAny}>Any</Button>
+                                            </li>
+                                            </ul>
+                                            // setButtonText('clicked');
+                                        ) : null}
+                                    </div>
+                                </div>
+
+                                <div className="d-grid gap-2">
+                                    <div className="dropdown">
+                                        <Button onClick={handleOpen2}>Model Size</Button>
+                                        {open2 ? (
+                                            <ul className="menu">
+                                            <li className="menu-item">
+                                                <Button onClick={SizeTiny}>Tiny</Button>
+                                            </li>
+                                            <li className="menu-item">
+                                                <Button onClick={SizeBase}>Base</Button>
+                                            </li>
+                                            <li className="menu-item">
+                                                <Button onClick={SizeSmall}>Small</Button>
+                                            </li>
+                                            <li className="menu-item">
+                                                <Button onClick={SizeMedium}>Medium</Button>
+                                            </li>
+                                            <li className="menu-item">
+                                                <Button onClick={SizeLarge}>Large</Button>
+                                            </li>
+                                            </ul>
+                                        ) : null}
+                                    </div>
+                                </div>
+                                <div style={{ padding: 20}}>
+                                    <form>
+                                        <label>Number of Speakers:   
+                                            <input type="number" value={num_spe} onChange={Num_Speakers} />
+                                        </label>
+                                    </form>
+                                </div>
+
                                 <div className="d-grid gap-2">
                                     <Button variant="secondary" size="lg" onClick={ updateFileUploaded }>
                                         Upload
